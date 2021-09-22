a4a4b63a0de72844c213571806040713775b521c
shreyalal15 <>
2021-09-22 18:50:25 +0000

    changes

--- index.js
+++ index.js
@@ -3,23 +3,10 @@ const chalk = require("chalk");
 
 console.log("")
 
-
-// const miles = 18;
-// const calculateFeet = miles => miles * 5280;
-
-// console.log(chalk`
-// 	There are {bold 5280 feet} in a mile.
-// 	In {bold ${miles} miles}, there are {green.bold ${calculateFeet(miles)} feet}.
-// `);
-
-// var play = readlineSync.keyInYN("do u like me?");
-// var play1 = readlineSync.keyInYN("really");
-
-
 var readlineSync = require('readline-sync');
 var userName = readlineSync.question("Whats your name?!  "); 
-console.log(chalk.yellow("\nWELCOME " +userName ,"to DO U KNOW SHREYA? "));
-console.log(chalk.magenta.bgWhite("(Enter your answers in lowercase letters)\n"));
+console.log(chalk.blue("\nWELCOME " +userName ,"to DO U KNOW SHREYA? "));
+console.log(chalk.magenta.bgWhite.italic("(Enter your answers in lowercase letters)\n"));
 score= 0;
 
 function hello(question, answer)
@@ -61,10 +48,6 @@ for(var i=0; i<levelzeroquestion.length; i++)
   hello(finalExecution.question, finalExecution.answer);
 }
 
-// hello("1] Where do I stay? ! ","manali");
-// hello("\n2] Where do I work? ! ","mumbai");
-// hello("\n3] What is my favourite food  ? ! ","momos");
-// hello("\n4] Whts my nickname?! ","pinky");
-// hello("\n5] whats is my job profile? ! ","manali");
+
 
 console.log("\n Your final score is: ", score );
